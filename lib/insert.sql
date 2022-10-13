d9257f7115c57c5a299a14b39379389d08dde13c
Yingyang Zhang <yingyang.zhang95@gmail.com>
2022-10-13 15:57:47 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 5, "M", "Black", "Strict", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 6, "M", "Brown", "Calm", 0);
+INSERT INTO bears VALUES (3, "Tabitha", 9, "F", "Black", "Native", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 10, "M", "Black", "Strict", 1);
+INSERT INTO bears VALUES (5, "Melissa", 5, "M", "Black", "Aggressive", 0);
+INSERT INTO bears VALUES (6, "Grinch", 3, "F", "Brown", "Nice", 1);
+INSERT INTO bears VALUES (7, "Wendy", 8, "M", "Black", "Strict", 0);
+INSERT INTO bears VALUES (8, null, 5, "F", "Dark Brown", "Quiet", 1);
